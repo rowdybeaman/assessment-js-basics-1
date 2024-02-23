0d3a5bcbfa12fb37ec91b1cbea6c054d8ed61887
Rowdy Beaman <rowdybeaman@gmail.com>
2024-02-23 10:59:19 -0700

    Variable finished

--- sdlc.js
+++ sdlc.js
@@ -12,6 +12,13 @@
 // Testing & Integration
 // Planning
 
+/////////////////////// Problem 1: ANSWER//////////////////
+// planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +26,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = `Assess requirements of project`
+const design = `Thinking about factors of the project to create correct specification, then preparing a proposal to implement`
+const implementation = `Beginning coding and `
+const testingAndIntegration = `Performing QA, and Launching the code (this is when it may go to DevOps)`
+const maintain = `Basic upkeep of the code to ensure it continues to run smoothly for the client`
