d8fc4c86fb9ac0b449b54291053edfb3ae68f51a
Rowdy Beaman <rowdybeaman@gmail.com>
2024-02-23 13:11:13 -0700

    Orchard Done

--- orchard.js
+++ orchard.js
@@ -51,14 +51,22 @@ const pinkPrice = .55
 
 // CODE HERE
 let totalAcres = 0
+// fuju 15 acres picked
+// gala 26 acres picked
+// pink 22 acres picked
+// total should log as 63
 
-for (let i = 0; i < 8; i++) {
-    totalAcres = fujiAcres[i] + galaAcres[i] + pinkAcres[i]
+for (let i = 0; i < fujiAcres.length; i++) {
+    totalAcres += fujiAcres[i] + galaAcres[i] + pinkAcres[i];
 }
-console.log(totalAcres)
+console.log(totalAcres);
 
+// NOTES: set the totalAcres to 0 so so i could use it to count 
+// when doing the for loop set i=0 as well, then i picked fujiAcers for the .length command but either of the other orchards would have worked as well. 
+// since totalAcres is just a sum, i added each index together as it was looping, by delcaring fujiacres[i] + the others in the same manner 
+// console log was just what totalAcres would be after the loop ran
 
-// PROBLEM 2
+///////////////////// PROBLEM 2 ////////////////////////
 
 /*
     Using `totalAcres`, calculate the average 
@@ -72,11 +80,15 @@ console.log(totalAcres)
 
 // CODE HERE
 
+totalAcres 
+// NOTES: totalAchres/7 would get average = 9
+// created new variable set it equal to total acres above and divided by how many days in the week (7)
+
+let averageDailyAcres = totalAcres/7
+console.log(averageDailyAcres)
 
 
-
-
-// PROBLEM 3
+/////////////// PROBLEM 3//////////////////
 
 /*
     We have provided 2 variables below. 
@@ -110,9 +122,19 @@ let days = 0
 
 // CODE HERE
 
+// NOTES: acresLeft/averageDailyAcres (174/9) = 19.33. I dont know how to express .33 days so im going to count that as a whole day so 19.33 rounded up to 20
+//  while loop needs to loop as long as there are acres left, so acresleft needs to be greater than 0
+// since the answer we are looking for is how many days itll take we will need to add 1 to days each time it loops, so days += 1
+// then we need to actually subtract daily average from how many acres are left so it doesnt just count days forever, but we need to do it every time it loops. so i used the -= so it would subtract each time the loop is run
+// then console log the answer we are looking for which we already noted would be how many days (rounded up to the nearest whole number) 
 
+while ( acresLeft > 0) {
+    days += 1
+    acresLeft -= averageDailyAcres
+}
+console.log(days)
 
-// PROBLEM 4
+/////////// PROBLEM 4 ///////////////////
 
 /*
     Your next task is to create 3 arrays
@@ -137,17 +159,37 @@ let days = 0
 */
 
 // CODE HERE
+// NOTES: Each day needs to be multiplied by 6.5 then pushed to the new arrays provided
+// const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
+// const galaAcres = [5, 2, 4, 3, 6, 2, 4]
+// const pinkAcres = [1, 5, 4, 2, 1, 5, 4]
+// i will then need to loop each index * 6.5 
 
-// let fujiTons =
-// let galaTons =
-// let pinkTons =
+// i first started by just focusing on fujiacres rather than all at once. 
 
 
+let fujiTons = []
+let galaTons = []
+let pinkTons = []
+
+// set up the for loop to run for the length of the array, then wanted to add the numbers to the new array fujiTons, so i used a push command, from there i multiplied fujiacres[i] by 6.5
 
 
+for ( let i = 0; i < fujiAcres.length; i++){
+    fujiTons.push(fujiAcres[i] * 6.5)
+    galaTons.push(galaAcres[i] * 6.5)
+    pinkTons.push(pinkAcres[i] * 6.5)
+}
+console.log(fujiTons, galaTons, pinkTons)
 
+// the hints actually confused me, i spent more time on this problem than any other trying to use the slice command but couldnt get it to work so i restarted and tried my own method
 
-// PROBLEM 5
+// answer for future use:
+// fujiTons = [13, 19.5, 19.5, 13, 13, 13, 6.5]
+// galaTons = [32.5, 13, 26, 19.5, 39, 13, 26]
+// pinkTons = [6.5, 32.5,   26, 13,  6.5, 32.5, 26]
+
+////////////// PROBLEM 5 ///////////////
 
 /*
     Next, calculate the total number of 
@@ -165,16 +207,24 @@ let days = 0
 
 // CODE HERE 
 
-// let fujiPounds =
-// let galaPounds =
-// let pinkPounds =
+// from a previous problem we know 63 total acres were picked, so i just created a new variable for each called totalsomethingAcres
+// fuju 15 acres picked
+// gala 26 acres picked
+// pink 22 acres picked
+
+const totalFujiAcres = 15
+const totalGalaAcres = 26
+const totalPinkAcres = 22
+
+let fujiPounds = totalFujiAcres * 2000
+let galaPounds = totalGalaAcres * 2000
+let pinkPounds = totalPinkAcres * 2000
+
+console.log(fujiPounds, galaPounds, pinkPounds)
 
 
 
-
-
-
-// PROBLEM 6
+//////////////// PROBLEM 6////////////////
 
 /*
     Now that you know the total pounds
@@ -192,16 +242,18 @@ let days = 0
 
 // CODE HERE
 
-// let fujiProfit =
-// let galaProfit =
-// let pinkProfit =
+// NOTES: 
+// const fujiPrice = .89 
+// const galaPrice = .64
+// const pinkPrice = .55
 
+let fujiProfit = fujiPrice * fujiPounds
+let galaProfit = galaPrice * galaPounds
+let pinkProfit = pinkPrice * pinkPounds
 
+console.log(fujiProfit, galaProfit, pinkProfit)
 
-
-
-
-// PROBLEM 7
+////////////// PROBLEM 7 ////////////////
 
 /*
     Add up all your profits and save 
@@ -212,3 +264,6 @@ let days = 0
 */
 
 // CODE HERE
+
+let totalProfit = fujiProfit + galaProfit + pinkProfit
+console.log(totalProfit)
